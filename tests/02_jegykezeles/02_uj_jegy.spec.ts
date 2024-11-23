fe6f721c97d061924d3740b748782aa1eb92169b
DEV\kormanyos.robert <kormanyos.robert@tigra.hu>
2024-11-23 18:50:31 +0100

    #2 task solution

---
+++ tests/02_jegykezeles/02_uj_jegy.spec.ts
@@ -0,0 +1,57 @@
+import { test, expect } from '@playwright/test';
+
+// Test Area: Login
+test.describe('Login Area', () => {
+  test('should successfully log in', async ({ page }) => {
+    await page.goto(''); // TODO: URL kiszervezése
+
+    // Login steps
+    await page.getByLabel('Login').fill(''); // TODO: Felhszanálónév kiszervezése
+    await page.getByLabel('Password').fill(''); // TODO: Jelszó kiszervezése
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    // Verify successful login
+    await expect(page.getByRole('link', { name: 'Kijelentkezés' })).toBeVisible();
+  });
+});
+
+// Test Area: Ticket Management
+test.describe('Ticket Management Area', () => {
+  test('should navigate to the project', async ({ page }) => {
+    // Login steps
+    await page.goto(''); // TODO: URL kiszervezése
+    await page.getByLabel('Login').fill(''); // TODO: Felhszanálónév kiszervezése
+    await page.getByLabel('Password').fill(''); // TODO: Jelszó kiszervezése
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    // Navigate to the project
+    await page.getByText('Ugrás projekthez...').click();
+    await page.getByRole('link', { name: 'Debreceni Egyetem 2024' }).first().click();
+    await expect(page.getByRole('link', { name: 'Feladatok' })).toBeVisible();
+  });
+
+  test('should create a new ticket', async ({ page }) => {
+    // Login and navigate to the project
+    await page.goto(''); // TODO: URL kiszervezése
+    await page.getByLabel('Login').fill(''); // TODO: Felhszanálónév kiszervezése
+    await page.getByLabel('Password').fill(''); // TODO: Jelszó kiszervezése
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.getByText('Ugrás projekthez...').click();
+    await page.getByRole('link', { name: 'Debreceni Egyetem 2024' }).first().click();
+    await page.getByRole('link', { name: 'Feladatok' }).click();
+    await page.getByRole('link', { name: 'Új feladat' }).click();
+
+    // Fill ticket form
+    await page.getByLabel('Tárgy *').fill('Ez az első feladatom');
+    await page.getByLabel('Leírás').fill('Ez az első feladat leírásom.');
+    await page.getByLabel('Felelős').selectOption('76');
+    await page.getByLabel('Befejezés dátuma').fill('2024-11-20');
+    await page.getByLabel('Becsült időigény').fill('0.5');
+
+    // Submit the ticket
+    await page.getByRole('button', { name: 'Létrehoz', exact: true }).click();
+
+    // Verify the ticket is created
+    await expect(page.getByText('Ez az első feladatom')).toBeVisible();
+  });
+});
